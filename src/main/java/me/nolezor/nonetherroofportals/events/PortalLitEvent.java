bc96054f5afb4f1afb22d066a0a28dfc847ba325
Lorenzo <88346895+Nolezor@users.noreply.github.com>
2023-04-08 19:32:40 +0200

    Update PortalLitEvent.java
    Now players can't ignite portals that are accessible from the roof even if they start under the it

--- src/main/java/me/nolezor/nonetherroofportals/events/PortalLitEvent.java
+++ src/main/java/me/nolezor/nonetherroofportals/events/PortalLitEvent.java
@@ -12,12 +12,12 @@ import org.bukkit.event.player.PlayerInteractEvent;
 public class PortalLitEvent implements Listener {
 
     @EventHandler
-    public void onPlayerPortalListTesti(PlayerInteractEvent e) {
+    public void onPlayerPortalLit(PlayerInteractEvent e) {
         Player p = e.getPlayer();
         if (!p.hasPermission("nonetherroofportals.bypass") && !(p.isOp() && NoNetherRoofPortals.opBypass) && NoNetherRoofPortals.litBlock) {
             if (p.getInventory().getItemInMainHand().getType() == Material.FLINT_AND_STEEL && p.getInventory().getItemInMainHand().getType() == Material.FLINT_AND_STEEL) {
                 if (e.getClickedBlock() != null) {
-                    if (e.getClickedBlock().getType() == Material.OBSIDIAN && e.getClickedBlock().getWorld().getEnvironment().equals(World.Environment.NETHER) && e.getClickedBlock().getLocation().getY() >= 127) {
+                    if (e.getClickedBlock().getType() == Material.OBSIDIAN && e.getClickedBlock().getWorld().getEnvironment().equals(World.Environment.NETHER) && e.getClickedBlock().getLocation().getY() >= 125) {
                         e.setCancelled(true);
                         p.sendMessage(ChatColor.RED + "You can't lit portals here.");
                     }
