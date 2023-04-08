46d9fabd44301515282116b9810a91a60247da8a
Lorenzo <88346895+Nolezor@users.noreply.github.com>
2023-04-08 19:36:35 +0200

    Update NoNetherRoofPortals.java

--- src/main/java/me/nolezor/nonetherroofportals/NoNetherRoofPortals.java
+++ src/main/java/me/nolezor/nonetherroofportals/NoNetherRoofPortals.java
@@ -1,6 +1,7 @@
 package me.nolezor.nonetherroofportals;
 
 import me.nolezor.nonetherroofportals.events.DestroyPortalEvent;
+import me.nolezor.nonetherroofportals.events.JoinRoofTeleportEvent;
 import me.nolezor.nonetherroofportals.events.ObsidianPlaceEvent;
 import me.nolezor.nonetherroofportals.events.PortalLitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,6 +13,7 @@ public final class NoNetherRoofPortals extends JavaPlugin {
     public static boolean obsidianBlock;
     public static boolean litBlock;
     public static boolean destroyPortal;
+    public static boolean teleportOnJoin;
 
     @Override
     public void onEnable() {
@@ -24,10 +26,12 @@ public final class NoNetherRoofPortals extends JavaPlugin {
         obsidianBlock = getConfig().getBoolean("Obsidian");
         litBlock = getConfig().getBoolean("Flint&Steel");
         destroyPortal = getConfig().getBoolean("DestroyExistingPortals");
+        teleportOnJoin = getConfig().getBoolean("ForceTeleportDown");
 
         getServer().getPluginManager().registerEvents(new ObsidianPlaceEvent(), this);
         getServer().getPluginManager().registerEvents(new PortalLitEvent(), this);
         getServer().getPluginManager().registerEvents(new DestroyPortalEvent(), this);
+        getServer().getPluginManager().registerEvents(new JoinRoofTeleportEvent(), this);
     }
 
     public static NoNetherRoofPortals getPlugin() {
