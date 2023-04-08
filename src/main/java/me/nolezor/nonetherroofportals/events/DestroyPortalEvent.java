cf52eb873c38e2cc641a81935b2283cfb2c84b7a
Lorenzo <88346895+Nolezor@users.noreply.github.com>
2023-04-08 19:30:16 +0200

    New Feature
    - Changed the Event to PlayerTeleportEvent (same effect with more options)
    - Added a check to destroy linked portals on the nether roof
    - Players who would have been teleported on the nether roof get teleported under it

--- src/main/java/me/nolezor/nonetherroofportals/events/DestroyPortalEvent.java
+++ src/main/java/me/nolezor/nonetherroofportals/events/DestroyPortalEvent.java
@@ -1,27 +1,43 @@
 package me.nolezor.nonetherroofportals.events;
 
 import me.nolezor.nonetherroofportals.NoNetherRoofPortals;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerPortalEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 public class DestroyPortalEvent implements Listener {
 
     @EventHandler
-    public void onPlayerEnterPortal(PlayerPortalEvent e) {
+    public void onPlayerEnterPortal(PlayerTeleportEvent e) {
         Player p = e.getPlayer();
-        if (!p.hasPermission("nonetherroofportals.bypass") && !(p.isOp() && NoNetherRoofPortals.opBypass) && NoNetherRoofPortals.destroyPortal) {
-            if (p.getWorld().getEnvironment().equals(World.Environment.NETHER) && p.getLocation().getY() >= 127) {
-                Block block = p.getLocation().getBlock();
-                if (block.getType() == Material.NETHER_PORTAL) {
-                    block.breakNaturally();
-                    e.setCancelled(true);
+        if (e.getCause() == PlayerTeleportEvent.TeleportCause.NETHER_PORTAL) {
+            if (!p.hasPermission("nonetherroofportals.bypass") && !(p.isOp() && NoNetherRoofPortals.opBypass) && NoNetherRoofPortals.destroyPortal) {
+                if (p.getWorld().getEnvironment().equals(World.Environment.NETHER) && p.getLocation().getY() >= 127) {
+                    Block block = p.getLocation().getBlock();
+                    if (block.getType() == Material.NETHER_PORTAL) {
+                        block.breakNaturally();
+                        e.setCancelled(true);
+                    }
+                } else if (p.getWorld().getEnvironment().equals(World.Environment.NORMAL)) {
+                    Location loc = e.getTo();
+                    if (loc.getY() >= 127) {
+                        if (loc.getBlock().getType() == Material.NETHER_PORTAL) {
+                            loc.getBlock().breakNaturally();
+                            Location safeLocation = JoinRoofTeleportEvent.searchSafeLocation(loc);
+                            p.teleport(safeLocation);
+                            safeLocation.setY(loc.getY()-1);
+                            JoinRoofTeleportEvent.generatePortal(safeLocation);
+                            e.setCancelled(true);
+                        }
+                    }
                 }
             }
         }
     }
+
 }
